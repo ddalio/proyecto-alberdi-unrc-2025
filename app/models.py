00ca91831f552b1be52db17df367b4049f786133
ddalio <magd45801@gmail.com>
2025-09-07 01:21:10 -0300

    más datos en seed y clientes funcionando

--- app/models.py
+++ app/models.py
@@ -8,9 +8,11 @@ class Cliente(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     telefono = db.Column(db.String(20))
+    institucion = db.Column(db.String(40))
     #activo = db.Column(db.Boolean, default=True)
     
     def __repr__(self):
         return f'<Cliente {self.dni} {self.nombre} {self.apellido}>' 
+    
 
 
